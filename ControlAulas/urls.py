7d12256c7e743ad6f4a894423caf4cf035a5da1d
diegoromero1 <70231529+diegoromero1@users.noreply.github.com>
2022-06-22 18:36:00 -0400

    conectar15

--- ControlAulas/urls.py
+++ ControlAulas/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls import url
 from django.views.static import serve
-from django.conf.urls import patterns, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +26,8 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     url(r'^download/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
 
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]
 if settings.DEBUG:
@@ -34,6 +35,5 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
