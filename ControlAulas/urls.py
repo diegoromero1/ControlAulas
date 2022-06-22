7936d642dbd25404605f2c54f96d05c37a2eaeb5
diegoromero1 <70231529+diegoromero1@users.noreply.github.com>
2022-06-22 18:17:01 -0400

    conectar7

--- ControlAulas/urls.py
+++ ControlAulas/urls.py
@@ -17,11 +17,17 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ControlAulasApp.urls')),
     path('', include('django.contrib.auth.urls')),
+    url(r'^download/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }),
+
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]
 # if settings.DEBUG:
